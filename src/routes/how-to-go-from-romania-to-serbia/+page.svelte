7ece69efa3a6b87d24262dbea42353eb23450f54
adamcolton <atcolton@tutanota.com>
2024-03-19 09:13:22 +0100

    romania to serbia

---
+++ src/routes/how-to-go-from-romania-to-serbia/+page.svelte
@@ -0,0 +1,68 @@
+<script>
+import MediaBox from '../MediaBox.svelte'
+</script>
+
+<svelte:head>
+	<title>How to go from Romania to Serbia</title>
+</svelte:head>
+
+<div class="rainbow-text-animated">
+	<h1>How to go from Romania to Serbia: A backpacker's guide</h1>
+</div>
+
+<MediaBox>
+	<img src="romania-to-serbia/marker.avif" />
+</MediaBox>
+
+March 18th, 2024
+
+<p>
+Serbia is located to the west of Romania. The large Romanian city Timisoara is located quite close to the Serbian border.
+Despite the proximity, it is not simple for travellers to book transportation into Serbia.
+</p>
+
+<MediaBox>
+	<img src="romania-to-serbia/serbia-map.jpg" />
+	It seems like it would be easy to travel from Timisoara to Serbia, but it isn't
+</MediaBox>
+
+<p>
+There are no international trains. There are buses but they are expensive. There are seats on passenger vans which apparantly cost 25 euros and require private booking through telegram. There is an alternative and it is to walk through the countryside for 15km.
+</p>
+
+<MediaBox>
+	<img src="romania-to-serbia/moravita-vrshatz.jpg" />
+</MediaBox>
+
+
+<p>
+You will be walking from the border crossing at Moravica to the medium sized Serbian town Вршац (pronounced Vershatz). Moravica is reachable by train from Timisoara with several running each day. The train is a decrepid single car and travels at about 30mph. The rail staff in Romania all have fantastic mustaches. To impress them grow a mustache of your own. The overall route is not infrequent for the intrepid and you may encounter some fellow travellers. No Romanian in their right mind crosses the border this way. 
+</p>
+
+<MediaBox>
+	<img src="romania-to-serbia/boys.avif" />
+	Thumbs up with the boys from Bangladesh
+</MediaBox>
+
+<p>
+ The crossing process starts right as you disembark at Moravita. The border man will question you briefly before taking you up to the checkpoint by van. The checkpoint is located on the highway. You will cross both the Romanian and Serbian checkpoints on foot but they will stamp your passport with a marking with a car symbol. Make sure to not have any bad drugs on you. This is for not only for your safe passage but also for the health and safety of the Serbian border guards who will open each of the pill capsules they find and test for illicit odors with a brusque sniffing.
+</p>
+
+<MediaBox>
+	<img src="romania-to-serbia/sign.avif" />
+	<img src="romania-to-serbia/walk.avif" />
+	bring sunscreen and bug spray
+</MediaBox>
+
+<p>
+After a brief fondling you will be let loose on the Serbian highway. It is not a miserable walk to Вршац. Hitchhiking is not illegal and is not looked down on. You may find a well intentioned driver to offer you a ride. You do not need to stick out your thumb but you can. Prepare your backpack in advance for hitchiking. Put your valuables in a small bag that you can keep on your lap and then your other more superfluous belongings in a bag you can throw haphazardly into the trunk. Drivers in Serbian cities are generally very careful but the blue collar drivers have frightening tendencies. Items in your throw bag may or may not get crushed or wet.
+</p>
+
+<MediaBox>
+	<img src="romania-to-serbia/fields.avif" />
+</MediaBox>
+
+<p>
+	With the language barrier you may find yourself being dropped off in some city on the outskirts of Belgrade. Inter city train schedules are intermittent and confusing . I'd recommend walking and there are abundant country walking paths to convey you to Belgrade. 
+</p>
+
